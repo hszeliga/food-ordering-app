cb148a2702103009d8d1ac23e7c1e6acdf0b0f39
hszeliga <hubert.szeliga@outlook.com>
2022-05-17 11:00:29 +0200

    Update URLs

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -89,7 +89,7 @@ const Product = ({product}) => {
 };
 
 export const getServerSideProps = async ({params}) => {
-  const res = await axios.get(`https://hszeligafood.vercel.app/api/products/${params.id}`);
+  const res = await axios.get(`/api/products/${params.id}`);
   return {
     props: {
       product: res.data,
